dcbabddc300f46992837d3f43967299da03d6b44
yzk <yzk0924@qq.com>
2022-06-05 14:39:14 +0800

    localStorageCache

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
+const localStorageCache = {};
 /**
  * 设置数据缓存
  * @param key 缓存key
  * @param val 缓存val
- * @param time 缓存时间（秒） 默认 1 小时；如果 time === 0 则默认没有过期时间
+ * @param time 缓存时间（秒） time === 0 则默认没有过期时间
  */
-const setCache = (key, val = "", time = 60 * 60) => {
+localStorageCache.set = (key, val, time = 0) => {
     let timestamp = (new Date()).valueOf();
+    let item;
     if (time === 0) {
-        uni.setStorageSync(key, val);
+        item = val;
     } else {
-        uni.setStorageSync(key, {
+        item = {
             data: val,
             expiration_time: timestamp + time * 1000
-        });
+        };
     }
-
+    if (localStorage != undefined) {
+        localStorage.setItem(key, JSON.stringify(item));
+    } else if (uni.getStorageSync != undefined) {
+        uni.setStorageSync(key, item);
+    } else if (wx.setStorageSync != undefined) {
+        wx.setStorageSync(key, item);
+    }
     return true;
-}
+};
 
 /**
  * 获取缓存
  * @param key 缓存key
  */
-const getCache = key => {
+localStorageCache.get = key => {
     let timestamp = (new Date()).valueOf();
     try {
-        let val = uni.getStorageSync(key);
+        let val;
+        if (localStorage != undefined) {
+            val = JSON.parse(localStorage.getItem(key));
+        } else if (uni.getStorageSync != undefined) {
+            val = uni.getStorageSync(key);
+        } else if (wx.getStorageSync != undefined) {
+            val = wx.getStorageSync(key);
+        }
         if (val.expiration_time === undefined) {
             //没有过期时间
             return val;
@@ -34,26 +49,41 @@ const getCache = key => {
             return val.data;
         } else {
             //已过期，清除之前缓存
-            uni.removeStorageSync(key);
+            del(key);
             return null;
         }
-    } catch (e) {
+    }
+    catch (e) {
         return null;
     }
-}
+};
 
 /**
- * 数据缓存函数
+ * 删除缓存
+ * @param key 缓存key
+ */
+localStorageCache.del = key => {
+    if (localStorage != undefined) {
+        localStorage.removeItem(key);
+    } else if (uni.removeStorageSync != undefined) {
+        uni.removeStorageSync(key);
+    } else if (wx.removeStorageSync != undefined) {
+        wx.removeStorageSync(key);
+    }
+};
+
+export default localStorageCache;
+
+/**
+ * 简单调用函数
  * @param key 缓存key
  * @param val 缓存val
  * @param time 缓存时间 默认 1 小时
  */
-const cache = (key, val = "", time = 3600) => {
-    if (val === "") {
-        return getCache(key);
+export const cache = (key, val = '', time = 3600) => {
+    if (val === '') {
+        return localStorageCache.get(key);
     } else {
-        return setCache(key, val, time)
+        return localStorageCache.set(key, val, time);
     }
-}
-
-export default cache
+};
